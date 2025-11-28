71d58902de93bbb0904e71b2244147807f8bbb90
TianYou.Zhan <tianyou.zhan@qiyucloud.com.cn>
2025-11-28 18:45:45 +0800

    optimize code

--- app-main/src/main/cpp/module/opensl/OpenSLESKit.h
+++ app-main/src/main/cpp/module/opensl/OpenSLESKit.h
@@ -1,21 +1,17 @@
 #ifndef ANDROID_AV_OPENSLESKIT_H
 #define ANDROID_AV_OPENSLESKIT_H
 
-#include <stdint.h>
+#include <cstdint>
 #include <string>
 #include <SLES/OpenSLES.h>
 #include <SLES/OpenSLES_Android.h>
 #include <log.h>
-
-//========================================== Common Utils ==========================================
 
 SLuint32 mapSlSampleRate(int32_t sampleRate);
 
 SLuint32 mapSlChannel(int channelCount);
 
 SLuint16 mapSlFormat(int bitsPerSample);
-
-//========================================== PCM Playback ==========================================
 
 struct PcmPlayerConfig {
     int32_t sampleRate;
@@ -31,7 +27,9 @@ int32_t commonPcmBufferSize(PcmPlayerConfig &pcmPlayerConfig, int durationPerBuf
 
 bool createPcmPlayer(PcmPlayerConfig &pcmConfig);
 
-/** 直接播放 PCM 文件，不会开启线程预读 PCM 数据，而是在 OpenSL ES 的回调中读取 PCM 数据并填充到队列中【相对比较低效】 */
+/** 直接播放 PCM 文件，不会开启线程预读 PCM 数据，
+ *  而是在 OpenSL ES 的回调中读取 PCM 数据并填充到队列中【相对比较低效】
+ */
 bool startPcmPlayerForFilePath(const char *filePath, PcmPlayConfig &pcmPlayConfig);
 
 bool pausePcmPlayer();
@@ -39,7 +37,5 @@ bool pausePcmPlayer();
 bool resumePcmPlayer();
 
 bool stopPcmPlayerForFilePath();
-
-//========================================== PCM Playback ==========================================
 
 #endif //ANDROID_AV_OPENSLESKIT_H
